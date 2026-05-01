62242e9e085caf62dbbb98d547637f3fdc86817b
Andrewjunior90 <89687051+Andrewjunior90@users.noreply.github.com>
2026-05-01 11:55:24 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = "drew-gadgets-v1";
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/logo.png.jpg"
+];
+
+// Install
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Activate (cleanup old cache)
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Fetch (serve from cache first)
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
